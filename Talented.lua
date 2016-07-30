3433fd85b8d74cb6a86e1594470da8bc72aaaddf
Tyler Swavely <tyler.r909@gmail.com>
2016-07-29 21:42:28 -0700

    ApplyTalents and GetActiveBuild built. Independently tested in-game. Error checking not implemented.
    Begin adding pseudocode to overall function

--- Talented.lua
+++ Talented.lua
@@ -1,1 +1,44 @@
 -- Talented Beta Build B-0.1
+
+--[[ LOAD_PARAMETERS
+Register when it opens
+    When player opens talent menu and switches to player talents
+        call initialize
+    Interact with user
+    Destroy Button on Exit
+--]]
+
+
+--[[ INITIALIZE
+    Load data into table?
+    create dropdown frame/button
+    lock frame to top left corner of talent frame
+    drop date into table
+--]]
+
+
+
+
+
+
+local function ApplyBuild(build)
+    for i = 1, GetMaxTalentTier() do
+        local s = build:sub(i,i)
+        --TODO: error checking
+        LearnTakents(GetTalentInfo(i,s,1))
+    end
+end
+
+
+local function GetActiveBuild()
+    local active_spec = ""
+
+    for row = 1,GetMaxTalentTier() do
+       for column = 1,3 do
+           local _,_,_,active = GetTalentInfo(row,column,1)
+           if active == true then active_spec = active_spec..column end
+       end
+    end
+
+    return active_spec
+end

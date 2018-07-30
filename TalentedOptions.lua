ad90e9f9f299c9d4160894354b6b0e22ab3d1c86
TylerR909 <drgmaster909@aim.com>
2018-07-30 16:00:53 -0700

    Added default class tables so the addon doesn't explode for real this time

--- TalentedOptions.lua
+++ TalentedOptions.lua
@@ -4,4 +4,23 @@ local defaultOptions = {}
 
 function Talented:InitOpts()
     self.db = LibStub("AceDB-3.0"):New("TalentedDB", defaultOptions, true)
+
+    -- GetNumSpecializations and GetSpecializationInfo don't return
+    -- anything on initialization (num=0, info=nil), so we need to
+    -- delay calling those until the player loads in
+    self:RegisterEvent("PLAYER_ENTERING_WORLD", function()
+        self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+        self:SeedDB()
+    end)
+end
+
+function Talented:SeedDB()
+    local classDB = self.db.class
+    for i=1, GetNumSpecializations(), 1 do
+        local specid = GetSpecializationInfo(i)
+        classDB[specid] = classDB[specid] or {
+            PvE = {},
+            PvP = {}
+        }
+    end
 end

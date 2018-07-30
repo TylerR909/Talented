cd9b8fe8e14e5de6e671cf27461c0d80a174f10a
TylerR909 <drgmaster909@aim.com>
2018-07-30 00:28:07 -0700

    Added default builds so the addon doesn't explode

--- TalentedDefaultSpecs.lua
+++ TalentedDefaultSpecs.lua
@@ -1,88 +1,86 @@
 Talented.IcyVeinsSpecs = {
     GameVersion = "8.0.1",
     -- DK - Blood
-    ["250"] = { },
+    ["250"] = { build = "0000000" },
     -- DK - Frost
-    ["251"] = { },
+    ["251"] = { build = "0000000" },
     -- DK - Unholy
-    ["252"] = { },
+    ["252"] = { build = "0000000" },
 
     -- DH - Havoc
-    ["577"] = { },
+    ["577"] = { build = "0000000" },
     -- DH - Vengeance
-    ["581"] = { },
+    ["581"] = { build = "0000000" },
 
     -- Druid - Balance
-    ["102"] = { },
+    ["102"] = { build = "0000000" },
     -- Druid - Feral
-    ["103"] = { },
+    ["103"] = { build = "0000000" },
     -- Druid - Guardian
-    ["104"] = { },
+    ["104"] = { build = "0000000" },
     -- Druid - Restoration
-    ["105"] = { },
+    ["105"] = { build = "0000000" },
 
     -- Hunter - Beast Mastery
-    ["253"] = { },
+    ["253"] = { build = "0000000" },
     -- Hunter - Marksmanship
-    ["254"] = { },
+    ["254"] = { build = "0000000" },
     -- Hunter - Survival
-    ["255"] = { },
+    ["255"] = { build = "0000000" },
 
     -- Mage - Arcane
-    ["62"] = { },
+    ["62"] = { build = "0000000" },
     -- Mage - Fire
-    ["63"] = { },
+    ["63"] = { build = "0000000" },
     -- Mage - Frost
-    ["64"] = { },
+    ["64"] = { build = "0000000" },
 
     -- Monk - Brewmaster
-    ["268"] = { },
+    ["268"] = { build = "0000000" },
     -- Monk - Windwalker
-    ["269"] = { },
+    ["269"] = { build = "0000000" },
     -- Monk - Mistweaver
-    ["270"] = { },
+    ["270"] = { build = "0000000" },
 
     -- Paladin - Holy
-    ["65"] = { },
+    ["65"] = { build = "0000000" },
     -- Paladin - Protection
-    ["66"] = { },
+    ["66"] = { build = "0000000" },
     -- Paladin - Retribution
-    ["70"] = { },
+    ["70"] = { build = "0000000" },
 
     -- Priest - Discipline
-    ["256"] = { },
+    ["256"] = { build = "0000000" },
     -- Priest - Holy
-    ["257"] = { },
+    ["257"] = { build = "0000000" },
     -- Priest - Shadow
-    ["258"] = { },
+    ["258"] = { build = "0000000" },
 
     -- Rogue - Assassination
-    ["259"] = { },
+    ["259"] = { build = "0000000" },
     -- Rogue - Outlaw
-    ["260"] = { },
+    ["260"] = { build = "0000000" },
     -- Rogue - Subtlety
-    ["261"] = { },
+    ["261"] = { build = "0000000" },
 
     -- Shaman - Elemental
-    ["262"] = { },
+    ["262"] = { build = "0000000" },
     -- Shaman - Enhancement
-    ["263"] = { },
+    ["263"] = { build = "0000000" },
     -- Shaman - Restoration
-    ["264"] = { },
+    ["264"] = { build = "0000000" },
 
     -- Warlock - Affliction
-    ["265"] = { },
+    ["265"] = { build = "0000000" },
     -- Warlock - Demonology
-    ["266"] = { },
+    ["266"] = { build = "0000000" },
     -- Warlock - Destruction
-    ["267"] = {
-        build = "2222021"
-     },
+    ["267"] = { build = "2222021" },
 
     -- Warrior - Arms
-    ["71"] = { },
+    ["71"] = { build = "0000000" },
     -- Warrior - Fury
-    ["72"] = { },
+    ["72"] = { build = "0000000" },
     -- Warrior - Protection
-    ["73"] = { },
+    ["73"] = { build = "0000000" },
 }

d76e95fb2c38e2b95b51fd0befdd7cf3c51f2308
TylerR909 <drgmaster909@aim.com>
2018-07-30 21:13:57 -0700

    Added basic squelching

--- TalentedOptions.lua
+++ TalentedOptions.lua
@@ -4,6 +4,7 @@ local defaultOptions = {}
 
 function Talented:InitOpts()
     self.db = LibStub("AceDB-3.0"):New("TalentedDB", defaultOptions, true)
+    self:InitSquelch()
 
     -- GetNumSpecializations and GetSpecializationInfo don't return
     -- anything on initialization (num=0, info=nil), so we need to
@@ -23,4 +24,33 @@ function Talented:SeedDB()
             PvP = {}
         }
     end
+end
+
+function Talented:InitSquelch()
+    self:AddSquelch()
+end
+
+function Talented:AddSquelch()
+    ChatFrame_AddMessageEventFilter("CHAT_MSG_SYSTEM", self.Squelch)
+end
+
+function Talented:RemoveSquelch()
+    ChatFrame_RemoveMessageEventFilter("CHAT_MSG_SYSTEM", self.Squelch)
+end
+
+local token = ".+"
+local unlearned = ERR_SPELL_UNLEARNED_S:format(token)
+local spell = ERR_LEARN_SPELL_S:format(token)
+local ability = ERR_LEARN_ABILITY_S:format(token)
+local passive = ERR_LEARN_PASSIVE_S:format(token)
+function Talented.Squelch(self, event, msg, ...)
+    if string.match(msg, unlearned) 
+        or string.match(msg, spell)
+        or string.match(msg, ability)
+        or string.match(msg, passive)
+    then
+        return true
+    end
+
+    return false
 end
